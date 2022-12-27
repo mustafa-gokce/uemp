f73717619adf630177fccd35ebeb9374954505f8
mustafa-gokce <mustafagokce009@gmail.com>
2022-12-27 10:41:38 +0300

    Fix socket option setting

--- UEMPC.cpp
+++ UEMPC.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv) {
     zmq::context_t ctx;
     zmq::socket_t sock(ctx, zmq::socket_type::sub);
     sock.connect("tcp://127.0.0.1:6666");
-    sock.set(zmq::sockopt::subscribe, "");
+    sock.setsockopt(ZMQ_SUBSCRIBE, "", 0);
 
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
