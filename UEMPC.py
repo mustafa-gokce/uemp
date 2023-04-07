bb677d5d7e798349352cb249e1b192aba5d93285
mustafa-gokce <mustafagokce@re-science.com>
2023-04-07 13:50:12 +0300

    bidirectional telemetry

--- UEMPC.py
+++ UEMPC.py
@@ -1,30 +1,82 @@
+import sys
 import time
+import threading
+import random
 import zmq
 import msgpack
 import UEMP
 
-# create ZeroMQ client
-context = zmq.Context()
-socket = context.socket(zmq.SUB)
-socket.connect("tcp://127.0.0.1:6666")
-socket.setsockopt(zmq.SUBSCRIBE, b"")
 
-# initialize counter
-i = -1
+def subscriber():
+    """This function is used to create a subscriber that receives messages from the publisher"""
 
-# infinite loop
-while True:
+    # create ZeroMQ client
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://127.0.0.1:6666")
+    socket.setsockopt(zmq.SUBSCRIBE, b"")
 
-    # increment counter
-    i += 1
+    # infinite loop
+    while True:
+        # receive the message
+        serialized = socket.recv()
 
-    # receive the message
-    serialized = socket.recv()
+        # deserialize the message
+        packed = msgpack.unpackb(serialized)
+        print(packed)
+        message = UEMP.MSG()
+        message.unpack(packed)
 
-    # deserialize the message
-    packed = msgpack.unpackb(serialized)
-    message = UEMP.MSG()
-    message.unpack(packed)
+        # print the message
+        print("Client:", round(time.time(), 3), "recv", message)
 
-    # print the message
-    print(i, message)
+
+def pusher(instance):
+    """This function is used to create a pusher that sends messages to the puller"""
+
+    # create ZeroMQ client
+    context = zmq.Context()
+    socket = context.socket(zmq.PUSH)
+    socket.connect("tcp://127.0.0.1:6667")
+
+    # infinite loop
+    while True:
+        # create a message object
+        message = UEMP.MSG(UEMP.MSG_ID.STATE_PLAYER,
+                           UEMP.STATE_PLAYER(instance, f"Player {instance}", random.randint(0, 100)))
+
+        # print the message
+        # print("Client:", round(time.time(), 3), "send", message)
+
+        # serialize the object with its method
+        packed = message.pack()
+        serialized = msgpack.packb(packed)
+
+        # send the message with no wait
+        socket.send(serialized, zmq.DONTWAIT)
+
+        # wait for some time
+        time.sleep(0.1)
+
+
+def main():
+    """This function is used to create the subscriber and the pusher"""
+
+    # get input arguments
+    instance = sys.argv[1]
+
+    # create the subscriber
+    subscriber_thread = threading.Thread(target=subscriber)
+    subscriber_thread.start()
+
+    # create the pusher
+    pusher_thread = threading.Thread(target=pusher, args=(instance,))
+    pusher_thread.start()
+
+    # wait for the threads to finish
+    subscriber_thread.join()
+    pusher_thread.join()
+
+
+if __name__ == "__main__":
+    main()
--- UEMPS.py
+++ UEMPS.py
@@ -1,43 +1,84 @@
 import time
+import threading
 import zmq
 import msgpack
 import UEMP
 
-# create ZeroMQ server
-context = zmq.Context()
-socket = context.socket(zmq.PUB)
-socket.bind("tcp://127.0.0.1:6666")
+# global variables
+players = {}
 
-# initialize counter
-i = -1
 
-# infinite loop
-while True:
+def publisher():
+    """This function is used to create a publisher that sends messages to the subscribers"""
 
-    # increment counter
-    i += 1
+    # create ZeroMQ server
+    context = zmq.Context()
+    socket = context.socket(zmq.PUB)
+    socket.bind("tcp://127.0.0.1:6666")
 
-    # create user message
-    if i % 2 == 0:
+    # infinite loop
+    while True:
 
-        # create a message object
-        message = UEMP.MSG(UEMP.MSG_ID.STATE_PLAYER, UEMP.STATE_PLAYER(9, "mustaphos", 26))
+        # send all the players
+        for player in players.keys():
+            # create a message object
+            message = UEMP.MSG(UEMP.MSG_ID.STATE_PLAYER, players[player])
 
-    # create npc message
-    else:
+            # print the message
+            # print("Server:", round(time.time(), 3), "send", message)
 
-        # create a message object
-        message = UEMP.MSG(UEMP.MSG_ID.STATE_NPC, UEMP.STATE_NPC(13, "depocu", 26, 10))
+            # serialize the object with its method
+            packed = message.pack()
+            serialized = msgpack.packb(packed)
 
-    # print the message
-    print(i, message)
+            # send the message with no wait
+            socket.send(serialized, zmq.DONTWAIT)
 
-    # serialize the object with its method
-    packed = message.pack()
-    serialized = msgpack.packb(packed)
+        # wait for some time
+        time.sleep(0.1)
 
-    # send the message with no wait
-    socket.send(serialized, zmq.DONTWAIT)
 
-    # wait for some time
-    time.sleep(0.1)
+def puller():
+    """This function is used to create a puller that receives messages from the pushers"""
+
+    # create ZeroMQ server
+    context = zmq.Context()
+    socket = context.socket(zmq.PULL)
+    socket.bind("tcp://127.0.0.1:6667")
+
+    # infinite loop
+    while True:
+        # receive the message
+        serialized = socket.recv()
+
+        # deserialize the object
+        packed = msgpack.unpackb(serialized)
+        message = UEMP.MSG()
+        message.unpack(packed)
+        player = message.msg
+
+        # print the message
+        print("Server:", round(time.time(), 3), "recv", player)
+
+        # find the player in the list or add it if it is not there
+        players[player.id] = player
+
+
+def main():
+    """This function is used to create the publisher and the puller"""
+
+    # create the publisher
+    publisher_thread = threading.Thread(target=publisher)
+    publisher_thread.start()
+
+    # create the puller
+    puller_thread = threading.Thread(target=puller)
+    puller_thread.start()
+
+    # wait for the threads to finish
+    publisher_thread.join()
+    puller_thread.join()
+
+
+if __name__ == "__main__":
+    main()
